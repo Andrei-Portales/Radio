7504d82666ab585de97eb5e5c1d69d49a355940b
andreiportales <andreiportales@macintosh>
2020-01-16 17:28:07 -0600

    agregar interface y clase

--- src/RadioInterface.java
+++ src/RadioInterface.java
@@ -1,6 +1,18 @@
 
 public interface RadioInterface {
 	
+	public void onOff();
 	
+	public void cambiarFrecuencia();
+	
+	public void avanzar();
+	
+	public void guardar(int boton);
+	
+	public void seleccionarEmisora(int boton);
+	
+	public String estacionActual();
+	
+	public boolean estado();
 
 }
