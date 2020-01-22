fb99d2307b071b398a2c2bfb95723e3a24fd1b1c
Andrei-Portales <30614183+Andrei-Portales@users.noreply.github.com>
2020-01-22 14:22:14 -0600

    Add files via upload

--- src/Radio.java
+++ src/Radio.java
@@ -1,69 +1,144 @@
-public class Radio 
-{
-  boolean estado;
-  boolean amFm; 
-  String[] estacionactual;
-  String[] guardadasAm;
-  String[] guardadasFm;
+import java.text.DecimalFormat;
 
-  public void OnOff(){
-  	if (estado){
-  		estado=false;
-  	}
-  	else{
-  		estado=true;
-  	}
-  }
 
-  public void cambiarFrecuencia(){
-  	if (amFm){
-  		amFm=false;
-  	}
-  	else{
-  		amFm=true;
-  	}
-  }
 
-  public void avanzar(){
-  	if (amFm){
-  		if (estacionactual[0]<1610){
-  			estacionactual[0]=estacionactual[0]+10;
-  		} else if (estacionactual[0]==1610){
-  			estacionactual[0]=530;
-  		}
-  	}
-  	else{
-  		if (estacionactual[1]<107.9){
-  			estacionactual[1]=estacionactual[1]+0.2;
-  		} else if (estacionactual[1]==107.9){
-  			estacionactual[1]=87.9;
-  		}
-  	}
-  } 
+public class Radio implements RadioInterface{
 
-  public void guardar(int boton){
-  	if (amFm){
-  		guardadasAm[boton]=estacionactual;
-  	}
-  	else{
-  		guardadasFm[boton]=estacionactual;
-  	}
-  }
-
-  public void seleccionarEmisora(int boton){
-  	if (amFm){
-  		estacionactual=guardadasAm[boton];
-  	}
-  	else{
-  		estacionactual=guardadasFm[boton];
-  	}
-  }
-
-  public float EstacionActual(){
-  	return estacionactual;
-  }
-
-  public boolean estado(){
-  	return estado;
-  }
+	private Integer[] frecuenciasAM;
+	private Double[] frecuenciasFM;
+	private boolean estado;
+	private String tipoFrecuencia;
+	
+	
+	private int am; 
+	private double fm;
+	
+	
+	
+	public Radio() {
+		frecuenciasAM = new Integer[12];
+		frecuenciasFM = new Double[12];
+		estado = false;
+		tipoFrecuencia = "FM";
+		am = 530;
+		fm =  87.9;
+	}
+	
+	
+	
+	
+	
+	/**
+	 * Funcion para encender y apagar el radio, funciona con estado booleanos
+	 */
+	@Override
+	public void onOff() {
+		// TODO Auto-generated method stub
+		
+		if (estado == false) {
+			estado = true;
+		}else if (estado == true) {
+			estado = false;
+		}
+		
+	}
+	
+	
+	/**
+	 * Funcion para cambiar de frecuencia de am a fm y viceversa
+	 */
+	@Override
+	public void cambiarFrecuencia() {
+		// TODO Auto-generated method stub
+		if (tipoFrecuencia.equals("FM")) {
+			tipoFrecuencia = "AM";
+		}else if (tipoFrecuencia.equals("AM")) {
+			tipoFrecuencia = "FM";
+		}
+	}
+	
+	
+	/**
+	 * Funcion para avanzar de frecuencia ya sea am o fm
+	 */
+	@Override
+	public void avanzar() {
+		// TODO Auto-generated method stub
+		if (tipoFrecuencia.equals("FM")) {
+			if (fm == 107.90000000000029 || fm == 107.9) {
+				fm = 87.9;
+			}else {
+				fm = fm + 0.2;
+			}
+		}else if (tipoFrecuencia.equals("AM")) {
+			if (am == 1610) {
+				am = 530;
+			}else {
+				am = am + 10;
+			}
+		}
+	}
+	
+	
+	/**
+	 * Funcion para guardar una frecuencia ya sea am o fm en boton indicado
+	 */
+	@Override
+	public void guardar(int boton) {
+		// TODO Auto-generated method stub
+		if (tipoFrecuencia.equals("FM")) {
+			frecuenciasFM[boton] = fm;
+		}else if(tipoFrecuencia.equals("AM")) {
+			frecuenciasAM[boton] = am;
+		}
+	}
+	
+	/**
+	 * funcion para seleccionar una emisora que ya fue guardada anteriormente
+	 */
+	@Override
+	public void seleccionarEmisora(int boton) {
+		// TODO Auto-generated method stub
+		try {
+		if (tipoFrecuencia.equals("FM")) {
+			fm = frecuenciasFM[boton];
+		}else if (tipoFrecuencia.equals("AM")) {
+			am = frecuenciasAM[boton];
+		}
+		}catch(Exception ex) {}
+		
+	}
+	
+	/**
+	 * funcion para poder dar a conocer la estacion que se esta sintonizando actulmente
+	 */
+	@Override
+	public String estacionActual() {
+		DecimalFormat df = new DecimalFormat("###.0");
+		// TODO Auto-generated method stub
+		String estacion = "";
+		if (tipoFrecuencia.equals("FM")) {
+			
+			estacion = ""+df.format(fm);
+		}else if (tipoFrecuencia.equals("AM")) {
+			estacion = "" + am;
+		}
+		
+		return estacion;
+	}
+	
+	
+	/**
+	 * Funcion para obtener el estado del radio, encendido o apagado
+	 */
+	@Override
+	public boolean estado() {
+		// TODO Auto-generated method stub
+		return estado;
+	}
+	
+	
+	
+	
+	
 }
