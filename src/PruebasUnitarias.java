01b94d78bad7fe80ff815ec6b5bf7162f68119b1
Andrei-Portales <30614183+Andrei-Portales@users.noreply.github.com>
2020-01-21 21:52:16 -0600

    Agregar metodos de Junit

--- src/PruebasUnitarias.java
+++ src/PruebasUnitarias.java
@@ -1,14 +1,41 @@
 import static org.junit.Assert.*;
+import static org.junit.Assume.assumeTrue;
 
 import org.junit.Test;
 
 public class PruebasUnitarias {
 
+	private Radio r = new Radio();
+	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test1() {
 		
+		r.estacionActual();
 		
 	}
+	
+	@Test
+	public void test2() {
+		
+		r.cambiarFrecuencia();
+		r.avanzar();
+		
+	}
+	
+	@Test
+	public void test3() {
+		
+		
+		r.guardar(0);
+		r.onOff();
+	}
+	
+	
+	
+	
+	
+	
+	
 
 }
